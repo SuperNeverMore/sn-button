6eb3e29533ca955d851d5f867d81b19a184a23f8
lilw7 <lilw7@yonyou.com>
2017-11-02 15:07:49 +0800

    initial

---
+++ examples/sn-button.vue
@@ -0,0 +1,13 @@
+<template>
+  <Demo></Demo>
+</template>
+
+<script>
+  import Demo from '../src/sn-button/sn-button';
+  export default {
+    name: 'app',
+    components: {
+      Demo
+    }
+  };
+</script>
